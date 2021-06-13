b1bb898eac8318b8da868c75e242a061137e9a18
sakshu2810 <66720465+sakshu2810@users.noreply.github.com>
2021-06-13 17:06:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+   
+<link rel="stylesheet" href="style.css">
+<style>
+body {
+  background-image: url('images/home.jpg');
+  height:100%;
+	
+
+    background-size: cover;
+	background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+</style>
+</head>
+
+<body style=" background-color: #c0c0c0;" >
+
+<div class="topnav" id="myTopnav">
+  <a href="#home" class="active" style=" background-color: #625D5D ;">Home</a>
+ <a href="transfermoney.php">Banking</a>
+  <a href="transactionhistory.php">History</a>
+  <a href="https://www.thesparksfoundationsingapore.org/">Sparks Foundation</a>
+  
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+<br>
+<section id="main" class="coloured-section">
+        <div class="container" style="padding-bottom: 50px; padding-top: 50px; color:white;">
+
+            <h1>
+                Welcome To Spark Foundation Bank !
+              
+               
+            </h1>
+        </div>
+    </section>
+    
+        
+        <br>
+    
+        <a href="transfermoney.php"><button type="button" class="btn btn-dark btn-lg download-button"></i> View Customers</button></a>
+    </section>
+    
+    </br>
+    </br>
+    </br>
+    </br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    
+
+
+<div class="foot">
+        <footer class="bg-light text-center text-lg-start">
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: white;text-align:center;color:black;">
+                © 2021 Copyright - Made by <strong>Sakshi Markad</strong> :
+                <a class="text-dark" href="https://www.thesparksfoundationsingapore.org/" target="_blank" style="color:white;"> The Sparks
+                    Foundation </a>
+            </div>
+
+
+    <footer id="footer" class="coloured-section">
+        <p>FIND US ON SOCIAL MEDIA</p>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <i class="social-icon fa fa-facebook-square" aria-hidden></i>
+        <i class="social-icon fa fa-instagram" aria-hidden="true"></i>
+        <i class="social-icon fa fa-twitter" aria-hidden="true"></i>
+        <i class="social-icon fa fa-linkedin" aria-hidden="true"></i>
+         </div>
+      </footer>
+
+
+
+<!-- 
+<script>
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+</script> -->
+</body>
+</html>
